ea4ca3dae92755537a6dfe4976bbb351a381c9d0
Luke Chui <chui.luke99@gmail.com>
2015-02-07 17:24:41 -0800

    heroku commit

---
+++ index.js
@@ -0,0 +1,27 @@
+$(function(){
+	console.log("ready");
+});
+
+//Company:
+
+// Pewter
+
+// Application Name:
+
+// Pewter
+
+// API Key:
+
+// 75wkul18hlrqm1
+
+// Secret Key:
+
+// 2tCQoiB8OAnCZHBz
+
+// OAuth User Token:
+
+// 958b1a1e-a845-4143-924b-8d1d89564a2f
+
+// OAuth User Secret:
+
+// 64b0473a-4b5a-4fda-9a72-626475ecdaf2
